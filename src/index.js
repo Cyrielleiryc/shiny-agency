3651df6f0cc49a8fc63eabc67950f4a3218aec6a
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-15 15:16:52 +0100

    learned how to use CSS in JS with library 'styled components'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
@@ -8,9 +10,16 @@ import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
 
+const GolbalStyle = createGlobalStyle`
+  * {
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+  }
+`
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GolbalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
