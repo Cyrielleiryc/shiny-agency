7535c418cf45ae4175895a45ba6380914d23a854
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-15 12:24:48 +0100

    added conditional routes for survey questions 'previous' and 'next'

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,8 +1,25 @@
+import { useParams, Link } from 'react-router-dom'
+
 function Survey() {
+  const { questionNumber } = useParams()
+  const questionNumberInt = parseInt(questionNumber)
+
+  const previousQuestion = (actualNumber) => {
+    return actualNumber === 1 ? '/survey/1' : `/survey/${actualNumber - 1}`
+  }
+
+  const nextQuestion = (actualNumber) => {
+    return actualNumber === 10 ? '/results' : `/survey/${actualNumber + 1}`
+  }
+
   return (
     <div>
       <h1>Questionnaire 🧮</h1>
+      <h2>Question {questionNumber}</h2>
+      <Link to={previousQuestion(questionNumberInt)}>Précédent</Link>
+      <Link to={nextQuestion(questionNumberInt)}>Suivant</Link>
     </div>
   )
 }
+
 export default Survey
