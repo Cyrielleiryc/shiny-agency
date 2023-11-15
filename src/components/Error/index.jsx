5bbfe3efadb3537a733c7de97357c885249ab61c
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-15 15:56:24 +0100

    styled the error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,32 @@
+import styled from "styled-components"
+import colors from "../../utils/style/colors"
+import error404 from "../../assets/404.svg"
+
+const ErrorContainer = styled.div`
+  margin: 30px;
+  background-color: ${colors.backgroundLight};
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`
+
+const ErrorText = styled.h1`
+  font-weight: 300;
+  font-size: 30px;
+  color: black;
+`
+
+const ErrorImage = styled.img`
+  maw-width: 800px;
+`
+
 function Error() {
   return (
-    <div>
-      <h1>Oups 🙈 Cette page n'existe pas</h1>
-    </div>
+    <ErrorContainer>
+      <ErrorText>Oups...</ErrorText>
+      <ErrorImage src={error404} alt= "error" />
+      <ErrorText>Il semblerait qu'il y ait un problème</ErrorText>
+    </ErrorContainer>
   )
 }
 
