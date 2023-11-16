05de0e018d8b24ad2c9c8c0dd3f559f4068401ca
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-16 16:05:19 +0100

    added context and used useContext for dark mode and answers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
 
 import Home from './pages/Home'
 import Survey from './pages/Survey'
@@ -9,25 +8,27 @@ import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
-
-const GolbalStyle = createGlobalStyle`
-  * {
-    font-family: 'Trebuchet MS', Helvetica, sans-serif;
-  }
-`
+import Footer from './components/Footer'
+import { ThemeProvider, SurveyProvider } from './utils/context'
+import GlobalStyle from './utils/style/GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <GolbalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
-// import { Loader } from '../../utils/style/Atoms'
+import { SurveyContext } from '../../utils/context'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -30,6 +31,31 @@ const LinkWrapper = styled.div`
   }
 `
 
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  cursor: pointer;
+  box-shadow: ${(props) =>
+    props.isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
@@ -37,18 +63,12 @@ function Survey() {
   const nextQuestionNumber = questionNumberInt + 1
   const [surveyData, setSurveyData] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const { answers, saveAnswers } = useContext(SurveyContext)
+  const [error, setError] = useState(false)
 
-//   useEffect(() => {
-//     setDataLoading(true)
-//     fetch(`http://localhost:8000/survey`)
-//       .then((response) => response.json())
-//       .then(({ surveyData }) => {
-//         setSurveyData(surveyData)
-//         setDataLoading(false)
-//         })
-//       .catch((error) => console.log(error))
-// }, [])
+  function saveReply(answer) {
+    saveAnswers({ [questionNumber]: answer })
+  }
 
   useEffect(() => {
     async function fetchSurvey() {
@@ -57,12 +77,10 @@ function Survey() {
         const response = await fetch(`http://localhost:8000/survey`)
         const { surveyData } = await response.json()
         setSurveyData(surveyData)
-      }
-      catch(err) {
+      } catch (err) {
         console.log(err)
         setError(true)
-      }
-      finally {
+      } finally {
         setDataLoading(false)
       }
     }
@@ -74,22 +92,36 @@ function Survey() {
   }
 
   return (
-      <SurveyContainer>
-          <QuestionTitle>Question {questionNumber}</QuestionTitle>
-          { isDataLoading ? (
-            <Loader />
-          ) : (
-            <QuestionContent>{surveyData[questionNumber]}   </QuestionContent>
-          )}
-          <LinkWrapper>
-              <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-              {surveyData[questionNumberInt + 1] ? (
-                  <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-              ) : (
-                  <Link to="/results">Résultats</Link>
-              )}
-          </LinkWrapper>
-      </SurveyContainer>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <ReplyWrapper>
+        <ReplyBox
+          onClick={() => saveReply(true)}
+          isSelected={answers[questionNumber] === true}
+        >
+          Oui
+        </ReplyBox>
+        <ReplyBox
+          onClick={() => saveReply(false)}
+          isSelected={answers[questionNumber] === false}
+        >
+          Non
+        </ReplyBox>
+      </ReplyWrapper>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
 
