664e4878084c54c9932ef66b5f0c47d2ebbf383b
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-16 15:06:54 +0100

    get data from api

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,26 +1,95 @@
+import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+// import { Loader } from '../../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+  const [surveyData, setSurveyData] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const previousQuestion = (actualNumber) => {
-    return actualNumber === 1 ? '/survey/1' : `/survey/${actualNumber - 1}`
-  }
+//   useEffect(() => {
+//     setDataLoading(true)
+//     fetch(`http://localhost:8000/survey`)
+//       .then((response) => response.json())
+//       .then(({ surveyData }) => {
+//         setSurveyData(surveyData)
+//         setDataLoading(false)
+//         })
+//       .catch((error) => console.log(error))
+// }, [])
 
-  const nextQuestion = (actualNumber) => {
-    return actualNumber === 10 ? '/results' : `/survey/${actualNumber + 1}`
+  useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
+        setSurveyData(surveyData)
+      }
+      catch(err) {
+        console.log(err)
+        setError(true)
+      }
+      finally {
+        setDataLoading(false)
+      }
+    }
+    fetchSurvey()
+  }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
   }
 
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={previousQuestion(questionNumberInt)}>Précédent</Link>
-      <Link to={nextQuestion(questionNumberInt)}>
-        {questionNumberInt === 10 ? 'Résultats' : 'Suivant'}
-      </Link>
-    </div>
+      <SurveyContainer>
+          <QuestionTitle>Question {questionNumber}</QuestionTitle>
+          { isDataLoading ? (
+            <Loader />
+          ) : (
+            <QuestionContent>{surveyData[questionNumber]}   </QuestionContent>
+          )}
+          <LinkWrapper>
+              <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+              {surveyData[questionNumberInt + 1] ? (
+                  <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+              ) : (
+                  <Link to="/results">Résultats</Link>
+              )}
+          </LinkWrapper>
+      </SurveyContainer>
   )
 }
 
